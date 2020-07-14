3000d6ce894d691e8801f3c9fc465095444fb81a
Kandimalla-Sanjana <kandimallasanjana@gmail.com>
2020-07-14 10:32:54 +0530

    Adding values from dictionary into an array

--- sorting.sh
+++ sorting.sh
@@ -6,3 +6,11 @@ op1=$(($a+$(($b*$c))))
 echo "a+b*c = " $op1
 op3=$(($c+$(($a/$b))))
 echo "c+a/b = " $op3
+counter=0
+for index in ${!operations[@]}
+do
+        array[((counter++))]=${operations[$index]}
+done
+
+echo ${array[@]}
+
