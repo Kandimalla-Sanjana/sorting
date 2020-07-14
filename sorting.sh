380d6e700ad285b7a377c8d68a577fa3cf7c1c76
Kandimalla-Sanjana <kandimallasanjana@gmail.com>
2020-07-14 09:37:53 +0530

    Operation 3 calculation

--- sorting.sh
+++ sorting.sh
@@ -2,5 +2,7 @@
 read -p "Enter value of a: " a
 read -p "Enter value of b: " b
 read -p "Enter value of c: " c
+op1=$(($a+$(($b*$c))))
+echo "a+b*c = " $op1
 op3=$(($c+$(($a/$b))))
 echo "c+a/b = " $op3
