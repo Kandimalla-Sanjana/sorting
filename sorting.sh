3b89ae6cddcf6fe29bdcebc1a3bac6a0629c72e6
Kandimalla-Sanjana <kandimallasanjana@gmail.com>
2020-07-14 09:48:22 +0530

    Reading computation values into dictionary

--- sorting.sh
+++ sorting.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+declare -A operations
 read -p "Enter value of a: " a
 read -p "Enter value of b: " b
 read -p "Enter value of c: " c
@@ -6,3 +7,13 @@ op1=$(($a+$(($b*$c))))
 echo "a+b*c = " $op1
 op3=$(($c+$(($a/$b))))
 echo "c+a/b = " $op3
+operations["a+b*c”]=$op1
+operations["a*b+c"]=$op2
+operations["c+a/b"]=$op3
+operations["a%b+c"]=$op4
+
+for index in ${!operations[@]}
+do
+        echo $index":"${operations[$index]}
+done
+
